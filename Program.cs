be5aa86200e0760377cf209acf2c3e7754676551
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-11-05 09:29:13 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿TimeSpan ts01 = new(10, 30, 00);
-TimeSpan ts02 = new(30, 20, 00);
+﻿using CA24110501;
 
-var ts03 = ts01 + ts02;
+Versenyzo v = new("Nagy Máté;1996;4;f;18-19;" +
+    "00:12:47;00:00:34;00:31:40;00:00:26;00:17:42");
 
+Console.WriteLine(v);
--- Versenyzo.cs
+++ Versenyzo.cs
@@ -9,5 +9,24 @@ public class Versenyzo
     public string Kategoria { get; set; }
     public Dictionary<string, TimeSpan> VersenyIdok { get; set; }
 
+    public override string ToString() => 
+        $"[{RajtSzam}] {Nev} ({Kategoria})";
 
+    public Versenyzo(string sor)
+    {
+        var v = sor.Split(';');
+        Nev = v[0];
+        SzulEv = int.Parse(v[1]);
+        RajtSzam = v[2];
+        Nem = v[3] == "f";
+        Kategoria = v[4];
+        VersenyIdok = new()
+        {
+            {"Úszás",    TimeSpan.Parse(v[5]) },
+            {"I. depó",  TimeSpan.Parse(v[6]) },
+            {"Kerékpár", TimeSpan.Parse(v[7]) },
+            {"II. depó", TimeSpan.Parse(v[8]) },
+            {"Futás",    TimeSpan.Parse(v[9]) },
+        };
+    }
 }
