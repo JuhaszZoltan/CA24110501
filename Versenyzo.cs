74868b37b200f7f3497432014c5e6b4666c61c21
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-11-05 08:49:17 +0100

    Add project files.

---
+++ Versenyzo.cs
@@ -0,0 +1,13 @@
+﻿namespace CA24110501;
+
+public class Versenyzo
+{
+    public string Nev { get; set; }
+    public int SzulEv { get; set; }
+    public string RajtSzam { get; set; }
+    public bool Nem { get; set; }
+    public string Kategoria { get; set; }
+    public Dictionary<string, TimeSpan> VersenyIdok { get; set; }
+
+
+}
